a8f37165014674f4fb39a1ec16e70d79ef957d48
joshi <shitov454@mail.ru>
2025-05-16 17:22:22 +0300

    added practicums

--- practicum_4/dfs.py
+++ practicum_4/dfs.py
@@ -14,7 +14,7 @@ class GraphTraversal(ABC):
         self.G: AnyNxGraph = G
         self.visited: set[Any] = set()
         self.reset()
-
+        
     def reset(self):
         self.visited.clear()
 
@@ -32,43 +32,32 @@ class GraphTraversal(ABC):
 
 
 class DfsViaRecursion(GraphTraversal):
-    def previsit(self, node: Any, **params) -> None:
-        print(f"Previsit {node}")
-
-    def postvisit(self, node: Any, **params) -> None:
-        print(f"Postvisit {node}")
     def run(self, node: Any) -> None:
+<<<<<<< HEAD
         self.previsit(node)
 <<<<<<< HEAD
         self.visited(node)
 =======
         self.visited.add(node)
 >>>>>>> c73b00f ([dfs_with_postvisit] create && [dijkstra.run()] was updated)
+=======
+>>>>>>> 19d698d (added practicums)
 
-        for n_neigh in G.neighbors(node):
-            if n_neigh not in self.visited:
-                self.run(n_neigh)
-        self.postvisit(node)
+        ##########################
+        ### PUT YOUR CODE HERE ###
+        #########################
 
+        pass
 
 
 class DfsViaLifoQueue(GraphTraversal):
-    def previsit(self, node: Any, **params) -> None:
-        print(f"Previsit {node}")
-    def postvisit(self, node: Any, **params) -> None:
-        print(f"Postvisit {node}")
     def run(self, node: Any) -> None:
 
-        stack = [node]
-        while len(stack) > 0:
-            node = stack.pop()
-            if node not in self.visited:
-                self.previsit(node)
-                self.visited.add(node)
+        ##########################
+        ### PUT YOUR CODE HERE ###
+        #########################
 
-                for n_neigh in G.neighbors(node):
-                    if n_neigh not in self.visited:
-                        stack.append(n_neigh)
+        pass
 
 
 class DfsViaRecursionWithPrinting(DfsViaRecursion):
@@ -107,32 +96,16 @@ class DfsViaLifoQueueWithPrinting(DfsViaLifoQueue):
         pass
 
 
-class TopologicalSorting(DfsViaRecursion):
-    def __init__(self, G: AnyNxGraph)->None:
-        self.sorted_nodes: deque = deque()
-        super().__init__(G)
-
-    def reset(self) -> None:
-        super().reset()
-        self.sorted_nodes.clear()
-
-    def sort(self, node: Any) -> list[Any]:
-        self.run(node)
-        sorted_nodes = list(self.sorted_nodes)
-        self.reset()
-        return sorted_nodes
-
-    def previsit(self, node: Any, **params) -> None:
-        pass
-
-    def postvisit(self, node: Any, **params) -> None:
-        self.sorted_nodes.appendleft(node)
+class TopologicalSorting(DfsViaLifoQueue):
+    ##########################
+    ### PUT YOUR CODE HERE ###
+    #########################
 
 
 if __name__ == "__main__":
     # Load and plot the graph
     G = nx.read_edgelist(
-        "simple_graph_10_nodes.edgelist",
+        Path("practicum_4") / "simple_graph_10_nodes.edgelist",
         create_using=nx.Graph
     )
     # plot_graph(G)
@@ -156,7 +129,7 @@ if __name__ == "__main__":
     # 3. Postorder recursive DFS for topological sort
     # If a directed graph represent tasks to be done, the topological sort tells
     # us what the task order should be, i.e. scheduling
-    G = nx.read_edgelist("simple_graph_10_nodes.edgelist", create_using=nx.DiGraph)
+    G = nx.read_edgelist(Path("practicum_4") / "simple_graph_10_nodes.edgelist", create_using=nx.DiGraph)
     print("Topological sorting")
     print("-" * 32)
     ts = TopologicalSorting(G)
